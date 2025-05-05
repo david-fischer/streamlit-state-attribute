ed53140e9d6a69a1b9b30c610d87d4173f1b9caa
David Fischer <d.fischer.git@posteo.de>
2025-05-05 19:13:02 +0200

    feat: initial commit

---
+++ src/examples/counter.py
@@ -0,0 +1,56 @@
+import logging
+from pathlib import Path
+
+import streamlit as st
+
+from st_state.state_property import StateProperty
+
+
+class Counter:
+    key: str
+    value: int = StateProperty(default=0, rerun="on_change")
+
+    def __init__(self, key: str) -> None:
+        self.key = key
+
+    def write(self) -> int:
+        """Render streamlit widgets for counter."""
+        with st.container(border=True):
+            st.text(self.key)
+            left, _, mid, _, right = st.columns([1, 4, 1, 4, 1])
+            with left:
+                if st.button(
+                    label="",
+                    icon=":material/remove:",
+                    key=self.key + "minus",
+                    type="primary",
+                ):
+                    self.value -= 1
+
+            with mid:
+                st.text(self.value)
+            with right:
+                if st.button(
+                    label="",
+                    icon=":material/add:",
+                    key=self.key + "plus",
+                    type="primary",
+                ):
+                    self.value += 1
+            return self.value
+
+
+def main() -> None:
+    logging.basicConfig(level=logging.INFO)
+    counter = Counter(key="counter_1")
+    counter.write()
+    counter2 = Counter(key="counter_2")
+    counter2.write()
+
+
+if __name__ == "__main__":
+    main()
+    with st.expander("Source Code"):
+        with Path(__file__).open(encoding="utf-8") as f:
+            code = f.read()
+        st.code(code, language="python")
---
+++ src/st_state/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ["StateProperty"]
+
+from state_property import StateProperty
